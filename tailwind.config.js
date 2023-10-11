e7fcfc7a4829e769a5a452bf6b2ca3016645cfd0
Su Lei <suleinandar3044@gmail.com>
2023-10-12 00:53:42 +0630

    to do app

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      fontFamily: {
+        heading: ['Montserrat', 'sans-serif'],
+        body: ['Rubik', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
